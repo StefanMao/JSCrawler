9ba606a1b60e785d610b8fdea97268c23d40d90d
stefanmao <stefanmao1112@gmail.com>
2021-12-09 17:11:57 +0800

    Modify test code

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ function onReadyStateChange() {
   console.log(this.status);
 
   if (this.readyState === 4 && this.status == 200) {
-    let { items } = JSON.parse(this.responseText);
-    dataArray.push(...formatData(items))
-    console.log(dataArray)
+    let { adjust, items } = JSON.parse(this.responseText);
+    maxPage = getPageLimit(adjust.count);
+    dataArray.push(...formatData(items));
+    page = page + 1;
+    console.log('page stateChange')
+    console.log(page);
   }
 }
 
@@ -30,31 +33,54 @@ function formatData(items) {
   return result;
 }
 
-function webCrawler(url) {
+function webCrawler(searchParam) {
   let xhr = new XMLHttpRequest();
   xhr.onerror = reqError;
   xhr.onreadystatechange = onReadyStateChange;
 
-
-  xhr.open("get", url, true);
-  xhr.send(null);
-
+  do {
+    console.log(page);
+    console.log(maxPage);
+    const url = transToApiUrl(searchParam, page);
+    xhr.open("get", url, true);
+    xhr.send(null);
+    console.log(page);
+  } while (page <= 5);
 }
 
-function getLocationUrl(){
- return location.href;
+function analyzeUrl(url) {
+  if (url.indexOf("?") != -1) {
+    let str = url.split("?");
+    let searchStr = str[1].split("&");
+    return searchStr[0];
+  }
+  return null;
+}
+
+function getPageLimit(count) {
+  let pageLimit = parseInt(count / 60);
+  return count % 60 !== 0 ? pageLimit : pageLimit - 1;
+}
+
+function transToApiUrl(searchParam, page) {
+  let newest = page * 60;
+  return `https://shopee.tw/api/v4/search/search_items?by=relevancy&${searchParam}&limit=60&newest=${newest}&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2`;
 }
 
 /** 目標網址 */
 // const TARGET_URL = "https://shopee.tw/search?keyword=nintendo%20switch";
+// const TARGET_URL =
+// "https://shopee.tw/api/v4/search/search_items?by=relevancy&keyword=nintendo%20switch&limit=60&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2";
 
+let dataArray = [];
+let page = 0;
+let maxPage = 0;
 
-const TARGET_URL =
-  "https://shopee.tw/api/v4/search/search_items?by=relevancy&keyword=nintendo%20switch&limit=60&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2";
+(function () {
+  const locationUrl = location.href;
+  const searchParam = analyzeUrl(locationUrl);
 
-  let dataArray = []
-  let page = 0
-  
-  console.log(getLocationUrl())
-  webCrawler(TARGET_URL);
-
+  if (searchParam) {
+    webCrawler(searchParam);
+  }
+})();
